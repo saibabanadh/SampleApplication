411905ec281d6ca26396573e8ed6c036c5b39283
saibabanadh <saibabanadh@gmail.com>
2014-07-02 13:01:14 +0530

    Authentication done (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.1.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
+gem 'bcrypt-ruby', '3.1.2'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
+  match '/test',    to: 'static_pages#test',    via: 'get'
+  
+  
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
